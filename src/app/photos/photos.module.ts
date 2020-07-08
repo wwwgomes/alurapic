4039520791f666c9343590975d68868c7e6f6118
Williams Gomes <williamsgomess@outlook.com>
2020-07-07 23:16:33 -0300

    Criando o primeiro componente

---
+++ src/app/photos/photos.module.ts
@@ -0,0 +1,9 @@
+import { NgModule } from "@angular/core";
+
+import { PhotoComponet } from "./photo/photo.component";
+
+@NgModule({
+  declarations: [ PhotoComponet ],
+  exports: [ PhotoComponet ]
+})
+export class  PhotosModule {}
