890da4276dfa95e5d401264b3677bbd033bb60b7
Williams Gomes <williamsgomess@outlook.com>
2020-07-12 22:27:02 -0300

    Melhorando a experiência do usuário
    LoadButton

--- src/app/photos/photos.module.ts
+++ src/app/photos/photos.module.ts
@@ -7,6 +7,7 @@ import { PhotoListComponent } from './photo-list/photo-list.component';
 import { PhotoFormComponent } from './photo-form/photo-form.component';
 import { PhotosComponent } from './photo-list/photos/photos.component';
 import { FilterByDescription } from './photo-list/filter-by-description.pipe';
+import { LoadButtonComponent } from './photo-list/load-button/load-button.component';
 
 @NgModule({
   declarations: [
@@ -15,6 +16,7 @@ import { FilterByDescription } from './photo-list/filter-by-description.pipe';
     PhotoFormComponent,
     PhotosComponent,
     FilterByDescription,
+    LoadButtonComponent,
   ],
   imports: [HttpClientModule, CommonModule],
 })
