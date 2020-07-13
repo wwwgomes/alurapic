a2de0caaf8cd89b76097216e5f749e218a07a5ee
Williams Gomes <williamsgomess@outlook.com>
2020-07-12 21:21:32 -0300

    Melhorando a experiência do usuário
    Pipe e implementação

--- src/app/photos/photos.module.ts
+++ src/app/photos/photos.module.ts
@@ -6,9 +6,16 @@ import { PhotoComponet } from './photo/photo.component';
 import { PhotoListComponent } from './photo-list/photo-list.component';
 import { PhotoFormComponent } from './photo-form/photo-form.component';
 import { PhotosComponent } from './photo-list/photos/photos.component';
+import { FilterByDescription } from './photo-list/filter-by-description.pipe';
 
 @NgModule({
-  declarations: [PhotoComponet, PhotoListComponent, PhotoFormComponent, PhotosComponent],
+  declarations: [
+    PhotoComponet,
+    PhotoListComponent,
+    PhotoFormComponent,
+    PhotosComponent,
+    FilterByDescription,
+  ],
   imports: [HttpClientModule, CommonModule],
 })
 export class PhotosModule {}
