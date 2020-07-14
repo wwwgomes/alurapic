adc17b95134e790d08e3eb65953221f0ac348978
Williams Gomes <williamsgomess@outlook.com>
2020-07-13 22:29:23 -0300

    Lapidando ainda mais nossa aplicação
    Component container e ng-content

--- src/app/photos/photo-list/photo-list.module.ts
+++ src/app/photos/photo-list/photo-list.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { CardModule } from '../../shared/components/card/card.module';
 import { PhotoListComponent } from './photo-list.component';
 import { PhotosComponent } from './photos/photos.component';
 import { LoadButtonComponent } from './load-button/load-button.component';
@@ -14,6 +15,6 @@ import { PhotoModule } from '../photo/photo.module';
     LoadButtonComponent,
     FilterByDescription,
   ],
-  imports: [CommonModule, PhotoModule],
+  imports: [CommonModule, PhotoModule, CardModule],
 })
 export class PhotoListModule {}
